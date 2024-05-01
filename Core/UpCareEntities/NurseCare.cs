ed51af6ca6f025d4cfda19fd0dacd3cea62d63f5
Fawaz110 <mmohamedfawzi23@gmail.com>
2024-05-02 00:37:15 +0300

    modify nurseCare table by adding to columns Bpm & Spo2

--- Core/UpCareEntities/NurseCare.cs
+++ Core/UpCareEntities/NurseCare.cs
@@ -6,6 +6,8 @@
         public string FK_PatientId { get; set; }
         public string FK_RoomId { get; set; }
         public string? Suger { get; set; }
+        public string? BeatPerMinute { get; set; }
+        public string? OxygenSaturation { get; set; }
         public string? BloodPresure { get; set; }
         public DateTime DateTime { get; set; } = DateTime.UtcNow;
         public string Notes { get; set; } = "No notes left. (Patient case is stable)";
